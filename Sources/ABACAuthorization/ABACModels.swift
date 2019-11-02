a4f7765e6dd2576d2441537ac1619f5859dbb50f
Leonid Orsulic <leonid.orsulic@nuvariant.com>
2019-11-02 10:05:36 +0100

    refactors model protocols to be public

--- Sources/ABACAuthorization/ABACModels.swift
+++ Sources/ABACAuthorization/ABACModels.swift
@@ -1,18 +1,18 @@
 import NIO
 import Foundation
 
-protocol ABACUser: Codable {}
+public protocol ABACUser: Codable {}
 
-protocol ABACRole: Codable {
+public protocol ABACRole: Codable {
     var name: String { get set }
 }
 
-protocol ABACUserData: Codable {
+public protocol ABACUserData: Codable {
     associatedtype ABACRoleType: ABACRole
     var roles: [ABACRoleType] { get set }
 }
 
-protocol ABACAccessData: Codable {
+public protocol ABACAccessData: Codable {
     associatedtype UserDataType: ABACUserData
     var userData: UserDataType { get set }
 }
