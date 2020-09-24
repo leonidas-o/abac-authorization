a07dd8dd9a103fdf05bb224de145fe8429025e4b
Leonid Orsulic <leonid.orsulic@nuvariant.com>
2020-09-24 23:17:56 +0200

    Add public initialiser for ABACCondition

--- Sources/ABACAuthorization/Models/ABACCondition.swift
+++ Sources/ABACAuthorization/Models/ABACCondition.swift
@@ -13,7 +13,29 @@ public struct ABACCondition: Codable {
     var rhs: String
     var authorizationPolicyID: UUID
     // optional extra fields
-    let csrfToken: String?
+    let _csrfToken: String?
+    
+    internal init(id: UUID? = nil,
+                  key: String,
+                  type: ABACConditionModel.ConditionValueType,
+                  operation: ABACConditionModel.ConditionOperationType,
+                  lhsType: ABACConditionModel.ConditionType,
+                  lhs: String,
+                  rhsType: ABACConditionModel.ConditionType,
+                  rhs: String,
+                  authorizationPolicyID: UUID,
+                  _csrfToken: String?) {
+        self.id = id
+        self.key = key
+        self.type = type
+        self.operation = operation
+        self.lhsType = lhsType
+        self.lhs = lhs
+        self.rhsType = rhsType
+        self.rhs = rhs
+        self.authorizationPolicyID = authorizationPolicyID
+        self._csrfToken = _csrfToken
+    }
 }
 
 
