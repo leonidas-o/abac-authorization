f8212d41f26d1047f602f0b8cb5f979d78c21a68
Leonid Orsulic <leonid.orsulic@nuvariant.com>
2020-10-18 15:17:53 +0200

    Extend factory to get repo from application

--- Sources/ABACAuthorization/Persistence/ABACAuthorizationPersistenceRepoFactory.swift
+++ Sources/ABACAuthorization/Persistence/ABACAuthorizationPersistenceRepoFactory.swift
@@ -6,6 +6,11 @@ public struct ABACAuthorizationPersistenceRepoFactory {
     public mutating func use(_ make: @escaping ((Request) -> ABACAuthorizationPersistenceRepo)) {
         self.make = make
     }
+    
+    var makeForApp: ((Application) -> ABACAuthorizationPersistenceRepo)?
+    mutating func useForApp(_ make: @escaping ((Application) -> ABACAuthorizationPersistenceRepo)) {
+        self.makeForApp = make
+    }
 }
 
 
@@ -26,6 +31,12 @@ extension Application {
 }
 
 
+
+extension Application {
+    var abacAuthorizationRepo: ABACAuthorizationPersistenceRepo {
+        self.abacAuthorizationRepoFactory.makeForApp!(self)
+    }
+}
 
 extension Request {
     public var abacAuthorizationRepo: ABACAuthorizationPersistenceRepo {
